19a3c999b5e4b9c99193a9d296f3f4b8582b9f12
ahsanrshah <ahsanrazasha@gmail.com>
2025-11-16 16:37:09 +0500

    Configure Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/First-React-App---Portfolio-Website/',
-  // ... rest of config
 })
